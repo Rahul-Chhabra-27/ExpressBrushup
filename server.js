32e2df8a6d98ddb7398fd05c12ad5cda8ad2fb1c
Rahul Chhabra <chhabrarahul2707@gmail.com>
2022-07-04 11:02:06 +0530

    server js updated

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config();
 const app = express();
 
 const name = 'John';
-app.get('/',(req,res) => res.send(`<h1>${name}</h1>`))
+app.use(express.static(path.join(__dirname,'static')));
 
 const PORT  = process.env.PORT || 5000;
 app.listen(PORT,() => console.log(`Server is in ${process.env.NODE_ENV} mode and running on port ${PORT}`));
